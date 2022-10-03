dacdabcdbf36b42bbb52fea0748a84facccba439
Ahsan Joiya <ahsan.joiya@appinsnap.com>
2022-10-03 15:04:20 +0500

    Added Leave Management Empty Controller

---
+++ src/WebUI/HRMS/Controllers/LeaveManagementController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRMS.Controllers;
+public class LeaveManagementController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
